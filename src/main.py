c5c47efc6e5df07c87b729bba7f33f1fbfbd898e
NightChips <nightchips64@gmail.com>
2020-04-15 13:08:48 +0300

    crypto

--- src/main.py
+++ src/main.py
@@ -41,10 +41,7 @@ async def echo(ctx, arg):
 async def ctf(ctx, *args):
     if args[0] in bot_commands.keys():
         try:
-<<<<<<< HEAD
             print(*args[1:])
-=======
->>>>>>> master
             xyz = bot_commands[args[0]](*args[1:])
             # TODO: check if xyz exceeds limit
             if type(xyz) == str:
